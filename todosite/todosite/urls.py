eee853970c4a1d73f576de5b6828a60ee4fed7d1
KarthihaKrishnan <karthiha.au@gmail.com>
2022-05-17 23:05:53 +0930

    some changes added in urls

--- todosite/todosite/urls.py
+++ todosite/todosite/urls.py
@@ -13,8 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from NotePad import views
 from django.contrib import admin
-from django.views import views
 from django.urls import path
 
 urlpatterns = [
