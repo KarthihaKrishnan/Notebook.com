c8295cc35859134523c53a2080ebff2eaa7733db
KarthihaKrishnan <karthiha.au@gmail.com>
2022-05-17 22:24:30 +0930

    Edited views our app

--- todosite/NotePad/views.py
+++ todosite/NotePad/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
+
+## import Notepad form and models
+
+from .forms import NotePadForm
+from .models import NotePad
+
+
+###############################################
+
+def index(request):
+    item_list = NotePad.objects.order_by("-date")
+    if request.method == "POST":
+        form = NotePadForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('NotePad')
+    form = NotePadForm()
+
+    page = {
+        "forms": form,
+        "list": item_list,
+        "title": "NOTEPAD LIST",
+    }
+    return render(request, 'NotePad/index.html', page)
+
+
+### function to remove item, it receive NotePad item_id as primary key from url ###
+def remove(request, item_id):
+    item = NotePad.objects.get(id=item_id)
+    item.delete()
+    messages.info(request, "item removed !!!")
+    return redirect('NotePad')
