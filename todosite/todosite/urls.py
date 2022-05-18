1ea519639c9b4880a119a5e8e3892f5396b113d0
KarthihaKrishnan <karthiha.au@gmail.com>
2022-05-18 09:38:06 +0930

    successfully functioned delete butten and items removed from list

--- todosite/todosite/urls.py
+++ todosite/todosite/urls.py
@@ -18,7 +18,11 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    #####################home_page###########################################
     path('', views.index, name="NotePad"),
-    path('del/', views.remove, name="del"),
+    ####################give id no. item_id name or item_id=i.id ############
+    # pass item_id as primary key to remove that the NotePad with given id
+    path('del/<str:item_id>', views.remove, name="del"),
+    ########################################################################
     path('admin/', admin.site.urls),
 ]
